e8e236b1c8d0392ad166af6ec468bc7cdd3e5ab8
SkittlesAt <ateev99@mail.ru>
2021-11-23 17:24:44 +0600

    Делаю дз 4 задание

--- CourseOne/main.swift
+++ CourseOne/main.swift
@@ -8,24 +8,57 @@
 import Foundation
 
 //№1 Написать функцию, которая определяет, четное число или нет.
+//
+//func isEvenNum(num: Int){
+//    if num % 2 == 0{
+//        print("Число четное")
+//    }
+//    else {
+//        print("Число не четное")
+//    }
+//}
+//
+//print("Введите целое число:")
+//if let number = readLine() {
+//    if let summaInt = Int(number){
+//        isEvenNum(num: summaInt)
+//    }
+//    else {
+//        print("Невозможно преоброзовать", number, "в число.")
+//    }
+//}
 
-func isEvenNum(num: Int){
-    if num % 2 == 0{
-        print("Число четное")
-    }
-    else {
-        print("Число не четное")
-    }
-}
 
-print("Введите целое число:")
-if let number = readLine() {
-    if let summaInt = Int(number){
-        isEvenNum(num: summaInt)
-    }
-    else {
-        print("Невозможно преоброзовать", number, "в число.")
-    }
-}
+// №2 Написать функцию, которая определяет, делится ли число без остатка на 3.
+//
+//func isEvenNum(num: Int) -> Bool{
+//    if num % 3 == 0{
+//        return true
+//    }
+//    else {
+//        return false
+//    }
+//}
+//
+//let number = 30
+//if isEvenNum(num: number) == true {
+//    print("Число делиться на 3 без остатка")
+//} else {
+//    print("Число не делиться на 3 без остатка")
+//}
+
+
+//№3 Создать возрастающий массив из 100 чисел.
+//
+//var numbers = [Int] ()
+//
+//for _ in 1...100 {
+//    let number = Int.random(in: 0..<10)
+//    numbers.append(number)
+//    print(numbers)
+//}
+
+
+//№4 Удалить из этого массива все четные числа и все числа, которые не делятся на 3.
 
 
